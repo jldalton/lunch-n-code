1aa0c83311fd08ae6e4fc938f5ceb13fbd442873
John Dalton <jldalton@containerstore.com>
2017-02-06 10:13:21 -0600

    Update to look up library

--- PerfectTemplate/Sources/main.swift
+++ PerfectTemplate/Sources/main.swift
@@ -3,7 +3,7 @@
 //  PerfectTemplate
 //
 //  Created by Kyle Jessup on 2015-11-05.
-//    Copyright (C) 2015 PerfectlySoft, Inc.
+//	Copyright (C) 2015 PerfectlySoft, Inc.
 //
 //===----------------------------------------------------------------------===//
 //
@@ -20,66 +20,93 @@
 import PerfectLib
 import PerfectHTTP
 import PerfectHTTPServer
+import Foundation
 
 // An example request handler.
 // This 'handler' function can be referenced directly in the configuration below.
 func handler(data: [String:Any]) throws -> RequestHandler {
+	return {
+		request, response in
+		// Respond with a simple message.
+		response.setHeader(.contentType, value: "text/html")
+		response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+		// Ensure that response.completed() is called when your processing is done.
+		response.completed()
+	}
+}
+
+func lookup_handler(data: [String:Any]) throws -> RequestHandler {
     return {
         request, response in
         // Respond with a simple message.
         response.setHeader(.contentType, value: "text/html")
-        response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+        
+        if let libname = request.param(name: "libname") {
+            let groupname = request.param(name: "group")!
+            let url = NSURL(string: "http://nexus.containerstore.com/nexus/service/local/artifact/maven/resolve?r=releases&g=\(groupname)&a=\(libname)&v=RELEASE")
+            let ns = NSData(contentsOf: url!.absoluteURL!)!
+            let nsxml = try! XMLDocument(data : ns as Data, options : 0)
+            let xmlString = try! nsxml.rootElement()?.nodes(forXPath: "//version")[0]
+            
+            response.appendBody(string: "<html><body>results: \(xmlString!)</body></html>")
+        } else {
+            response.appendBody(string: "<html><title>Hello, lib!</title><body>Missing libname!</body></html>")
+        }
         // Ensure that response.completed() is called when your processing is done.
         response.completed()
     }
 }
 
+// http://nexus.containerstore.com/nexus/service/local/artifact/maven/resolve?r=central&g=junit&a=junit&v=RELEASE
+
 // Configuration data for two example servers.
-// This example configuration shows how to launch one or more servers
+// This example configuration shows how to launch one or more servers 
 // using a configuration dictionary.
 
 let port1 = 8080, port2 = 8181
 
 let confData = [
-    "servers": [
-        // Configuration data for one server which:
-        //    * Serves the hello world message at <host>:<port>/
-        //    * Serves static files out of the "./webroot"
-        //        directory (which must be located in the current working directory).
-        //    * Performs content compression on outgoing data when appropriate.
-        [
-            "name":"localhost",
-            "port":port1,
-            "routes":[
-                ["method":"get", "uri":"/", "handler":handler],
-                ["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.staticFiles,
-                 "documentRoot":"./webroot",
-                 "allowResponseFilters":true]
-            ],
-            "filters":[
-                [
-                "type":"response",
-                "priority":"high",
-                "name":PerfectHTTPServer.HTTPFilter.contentCompression,
-                ]
-            ]
-        ],
-        // Configuration data for another server which:
-        //    * Redirects all traffic back to the first server.
-        [
-            "name":"localhost",
-            "port":port2,
-            "routes":[
-                ["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.redirect,
-                 "base":"http://localhost:\(port1)"]
-            ]
-        ]
-    ]
+	"servers": [
+		// Configuration data for one server which:
+		//	* Serves the hello world message at <host>:<port>/
+		//	* Serves static files out of the "./webroot"
+		//		directory (which must be located in the current working directory).
+		//	* Performs content compression on outgoing data when appropriate.
+		[
+			"name":"localhost",
+			"port":port1,
+			"routes":[
+				["method":"get", "uri":"/", "handler":handler],
+				["method":"get", "uri":"/look-up-lib", "handler":lookup_handler],
+				["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.staticFiles,
+				 "documentRoot":".",
+				 "allowResponseFilters":true]
+			],
+			"filters":[
+				[
+				"type":"response",
+				"priority":"high",
+				"name":PerfectHTTPServer.HTTPFilter.contentCompression,
+				]
+			]
+		],
+		// Configuration data for another server which:
+		//	* Redirects all traffic back to the first server.
+		[
+			"name":"localhost",
+			"port":port2,
+			"routes":[
+				["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.redirect,
+				 "base":"http://localhost:\(port1)"]
+			]
+		]
+	]
 ]
 
 do {
-    // Launch the servers based on the configuration data.
-    try HTTPServer.launch(configurationData: confData)
+	// Launch the servers based on the configuration data.
+	try HTTPServer.launch(configurationData: confData)
 } catch {
-    fatalError("\(error)") // fatal error launching one of the servers
+	fatalError("\(error)") // fatal error launching one of the servers
 }
+
